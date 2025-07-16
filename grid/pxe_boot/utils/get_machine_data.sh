1fef1b950e4cddfbe5055dfe460ab4d4f81b70c2
Yannick Patois <yannick.patois@iphc.cnrs.fr>
2025-07-16 16:26:31 +0200

    Using parallel to speed up data gathering

--- grid/pxe_boot/utils/get_machine_data.sh
+++ grid/pxe_boot/utils/get_machine_data.sh
@@ -2,7 +2,16 @@
 
 mkdir -p data
 
+function get_one_machine_data() {
+    echo "For machine: $wn"
+    ssh -o ConnectTimeout=2 root@${wn} "ip a" > ./data/$wn.txt
+}
+
+export -f get_one_machine_data
+
+WNlist=""
 for wn in `grep sbgwn ../vault/ansible_data/hosts  | grep -v '#'`; do
-    echo "Machine: $wn"
-    ssh -o ConnectTimeout=2 $wn "ip a" > ./data/$wn.txt
+    WNlist="$WNlist $wn"
 done
+
+parallel --line-buffer --jobs 64 get_one_machine_data ::: $WNlist
